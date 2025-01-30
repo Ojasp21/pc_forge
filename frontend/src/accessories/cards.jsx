b101d0c98719dbfde2a0eb4b78489dfb53c00e71
Ojasp21 <ojasp21@gmail.com>
2025-01-30 20:18:03 +0530

    added accesories

--- frontend/src/accessories/cards.jsx
+++ frontend/src/accessories/cards.jsx
@@ -7,11 +7,11 @@ export default function Cards({ title }) {
       <h3 id="type">{title}</h3>
 
       {/* Buttons Section */}
-      <div className="buttons">
+      {/* <div className="buttons">
         <button className="btn buy-now">Buy Now</button>
         <button className="btn add-to-cart">Add to Cart</button>
         <button className="btn more-details">More Details</button>
-      </div>
+      </div> */}
     </div>
   );
 }
