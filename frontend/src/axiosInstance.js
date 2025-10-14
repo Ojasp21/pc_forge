75644eb82312c6dfec21df00f0f5f4c9e3150f7f
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-10-14 17:05:22 +0530

    test

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 // src/axiosInstance.js
 import axios from 'axios';
 
-console.log("Backend URL →", import.meta.env.VITE_BACKEND_URL);
+console.log("Backend URL →", import.meta.env.VITE_BACKEND_URI);
 // Create an axios instance with the base URL pointing to your backend
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URI || "https://pc-forge-backend.vercel.app/api", // Replace with your backend URL
