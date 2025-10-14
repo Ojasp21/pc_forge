d0b42ffb950cbdcfd20bb19f3052bdb7b4bdbdac
Santrupt Potphode <santrupt.potphode29@gmail.com>
2025-10-14 17:02:02 +0530

    Compatibility check fixed

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an axios instance with the base URL pointing to your backend
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URI, // Replace with your backend URL
+  baseURL: import.meta.env.VITE_BACKEND_URI || "https://pc-forge-backend.vercel.app/api", // Replace with your backend URL
   withCredentials: true,
 });
 
